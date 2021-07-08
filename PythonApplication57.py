5f600c931aec20dfa7d810ded0b972941d6edca8
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2021-07-09 03:32:04 +0900

    Python언어 for문 연습!(4)

--- PythonApplication57.py
+++ PythonApplication57.py
@@ -1,11 +1,11 @@
-list1 = []
-N = int(input('입력받을 정수의 개수를 입력하시오 : '))
-if N >1:
-    for x in range(N):
-        a = int(input("정수를 입력하시오 : "))
-        list1.append(a)
+list1 = [] # 리스트 list1 생성
+N = int(input('입력받을 정수의 개수를 입력하시오 : ')) # 사용자로부터 정수 입력받기
+if N >1: # 만약 N이 1보다 크다면
+    for x in range(N): # x에 0부터 N-1까지 대입하며 반복
+        a = int(input("정수를 입력하시오 : ")) # 사용자로부터 정수 입력받기
+        list1.append(a) # 입력받은 정수를 리스트 list1에 추가하기
 
-    M = int(input("정수 M을 입력하시오 : "))
+    M = int(input("정수 M을 입력하시오 : ")) # 사용자로부터 정수 입력받기
 
     for x in range(N):
         if list1[x] < M:
